8685c969586f2889691837335e009a57ef0fc837
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-22 14:39:59 +0900

    feat(entity): SoilMoisture 엔티티 추가

---
+++ src/main/java/capstone/smartfarm/model/entity/SoilMoisture.java
@@ -0,0 +1,21 @@
+package capstone.smartfarm.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "soil_moisture_data", indexes = {
+        @Index(name = "idx_soil_moisture_measured_at", columnList = "measuredAt")
+})// 시간으로 조회 빈번하므로 인덱싱
+public class SoilMoisture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private float value;
+
+    @Column(nullable = false)
+    private LocalDateTime measuredAt = LocalDateTime.now();
+}
