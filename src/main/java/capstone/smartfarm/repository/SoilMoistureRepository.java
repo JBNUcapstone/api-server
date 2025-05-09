50c692d2da6d54c0dd1c601759fdd63e09086f47
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-09 17:27:33 +0900

    feat: SoilMoisture,Temperature 커스텀 JPA 메소드 추가

--- src/main/java/capstone/smartfarm/repository/SoilMoistureRepository.java
+++ src/main/java/capstone/smartfarm/repository/SoilMoistureRepository.java
@@ -1,7 +1,12 @@
 package capstone.smartfarm.repository;
 
 import capstone.smartfarm.model.entity.SoilMoisture;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface SoilMoistureRepository extends JpaRepository<SoilMoisture, Integer> {
+    Page<SoilMoisture> findAllByMeasuredAtBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
 }
--- src/main/java/capstone/smartfarm/repository/TemperatureRepository.java
+++ src/main/java/capstone/smartfarm/repository/TemperatureRepository.java
@@ -1,8 +1,12 @@
 package capstone.smartfarm.repository;
 
 import capstone.smartfarm.model.entity.Temperature;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TemperatureRepository extends JpaRepository<Temperature, Integer> {
+import java.time.LocalDateTime;
 
+public interface TemperatureRepository extends JpaRepository<Temperature, Integer> {
+    Page<Temperature> findAllByMeasuredAtBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
 }
